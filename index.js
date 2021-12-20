10fd26d7425ecb14600a4300be40eef9cd054e6c
Gino Tasis <gtasis84@gmail.com>
2021-12-20 16:04:19 -0500

    First init

---
+++ index.js
@@ -0,0 +1,18 @@
+// Data Type Conversion
+
+/*
+Boolean - Boolean()
+Number - Strict: "+" and Number() and LOOSE: parseInt()
+String - toString()
+*/
+
+let booleanNumber = 56;
+// console.log(Boolean(booleanNumber))
+
+
+let booleanString = ""
+// console.log(Boolean(booleanString))
+
+let numberString = ""
+console.log(Number(numberString))
+
